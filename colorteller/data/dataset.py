daad881a16e71dfe6e98e0d82fcc3af51b422cc9
LM <hi@leima.is>
2021-11-24 21:43:36 +0100

    added first dataset class

---
+++ colorteller/data/dataset.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+
+
+class Dataset:
+    def __init__(self) -> None:
+        pass
+
+
+class TwoDimScalar(Dataset):
+    def __init__(self, ranks=None, index=None, limits=None):
+        super().__init__()
+
+        if ranks is None:
+            ranks = 2
+        if index is None:
+            index = ['x', 'y']
+        if limits is None:
+            limits = [0, 100]
+
+        if not isinstance(ranks, int):
+            raise TypeError("ranks must be a int")
+        if not isinstance(index, (list, tuple)):
+            raise TypeError("index must be a list or tuple")
+        if not isinstance(limits, (list, tuple)):
+            raise TypeError("limits must be a list or tuple")
+
+        self.dims = ranks
+        self.limits = limits
+        self.index = index
+        self.columns = self._columns()
+
+    def _columns(self):
+        column_names = []
+
+        for i in range(self.ranks):
+            column_names.append(f"col_{i}")
+
+        return column_names
+
+    def data(self):
+        data = self._rand_data()
+        dataframe = pd.DataFrame(data, columns=self.columns)
+
+        return dataframe
+
+    def _rand_data(self):
+        return np.random.rand(self.ranks, self.rows).tolist()
+
+    @property
+    def rows(self):
+        return len(self.index)
+
+    @property
+    def min(self):
+        return self.limits[0]
+
+    @property
+    def max(self):
+        return self.limits[1]
+
+    def __len__(self):
+        return self.rows
