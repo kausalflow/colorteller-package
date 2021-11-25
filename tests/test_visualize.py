58465f78b42fb32d975deb408a0ce4c670a6ab24
LM <hi@leima.is>
2021-11-25 13:53:57 +0100

    added applicationcharts barchart

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,6 +1,6 @@
 from colorteller import teller
 from colorteller.utils import benchmark
-from colorteller.visualize import BenchmarkCharts
+from colorteller.visualize import BenchmarkCharts, ApplicationCharts
 from loguru import logger
 from nose import tools as _tools
 
@@ -21,3 +21,15 @@ def test__visualize___Charts():
     charts.distance_matrix(show=False)
 
     charts.noticable_matrix(show=False)
+
+
+def test__visualize___Charts():
+    hex_strings = ["#8de4d3", "#344b46", "#74ee65", "#238910", "#a6c363", "#509d99"]
+
+    ct = teller.ColorTeller(hex_strings=hex_strings)
+
+    c = teller.Colors(colorteller=ct)
+
+    ac = ApplicationCharts(colors=c, save_folder=".")
+
+    ac.bar_chart(show=True)
