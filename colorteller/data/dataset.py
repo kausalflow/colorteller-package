41609ee89dd1e1b82f2775b5235a72ea3959a1fe
LM <hi@leima.is>
2021-11-25 17:30:02 +0100

    added applicatioin charts

--- colorteller/data/dataset.py
+++ colorteller/data/dataset.py
@@ -78,3 +78,66 @@ class TwoDimScalar(Dataset):
 
     def __len__(self):
         return self.rows
+
+
+class OneDimScalar(Dataset):
+    def __init__(self, index=None, limits=None, seed=None, mode=None):
+        super().__init__()
+
+        if index is None:
+            index = 5
+        if limits is None:
+            limits = [0, 100]
+
+        if isinstance(index, int):
+            index = self._indices(index)
+        elif not isinstance(index, (list, tuple)):
+            raise TypeError("index should be int, list or tuple")
+        if not isinstance(limits, (list, tuple)):
+            raise TypeError("limits must be a list or tuple")
+
+        if mode is None:
+            mode = "rand"
+
+        self.limits = limits
+        self.index = index
+        self.seed = seed
+        self.mode = mode
+
+    def _indices(self, length):
+        index = []
+
+        for i in range(length):
+            index.append(f"idx_{i}")
+
+        return index
+
+    def data(self):
+        data = self._rand_data()
+        series = pd.Series(data, index=self.index)
+
+        return series
+
+    def _rand_data(self):
+        if self.mode == "rand":
+            np.random.seed(self.seed)
+            return np.random.rand(self.rows)
+        elif self.mode == "constant":
+            return np.ones(self.rows) * self.max
+        else:
+            raise ValueError("mode should be: rand, linear or constant")
+
+    @property
+    def rows(self):
+        return len(self.index)
+
+    @property
+    def min(self):
+        return self.limits[0]
+
+    @property
+    def max(self):
+        return self.limits[1]
+
+    def __len__(self):
+        return self.rows
