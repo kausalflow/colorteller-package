764c84e79d1dc3c3cb269ae5da660ff338923d01
LM <hi@leima.is>
2021-11-25 11:58:13 +0100

    adjust two d random gen

--- tests/data/test_dataset.py
+++ tests/data/test_dataset.py
@@ -1,7 +1,9 @@
 import colorteller.data.dataset as dataset
+from loguru import logger
 
 
 def test__TwoDimScalar():
     ds = dataset.TwoDimScalar()
+    ds_1 = dataset.TwoDimScalar(index=6)
 
-
+    logger.debug(ds_1.data().head())
